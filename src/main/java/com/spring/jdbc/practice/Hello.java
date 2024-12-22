834a1ac35ccc3acf6161cf629223753935ac1857
ArshadAhmad123 <140145067+ArshadAhmad123@users.noreply.github.com>
2024-12-22 18:10:25 +0530

    practicing

---
+++ src/main/java/com/spring/jdbc/practice/Hello.java
@@ -0,0 +1,9 @@
+package com.spring.jdbc.practice;
+
+public class Hello {
+    private int a;
+    private int b;
+    public static void main(String[] args) {
+		System.out.println("I am learning new things");
+	}
+}
